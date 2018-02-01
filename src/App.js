0c3e03301346ffb6a004b20acbd55cead7982f87
Joe Lau <mcfa77y@gmail.com>
2018-02-01 09:52:30 -0800

    got font awesome working

--- src/App.js
+++ src/App.js
@@ -1,20 +1,28 @@
-import React from 'react'
-import {
-  BrowserRouter as Router,
-  Route,
-  Link
-} from 'react-router-dom'
-import Home from './Home'
+import React from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+
+import 'bootstrap/dist/css/bootstrap.css';
+import './third-party/font-awesome/css/fontawesome.min.css';
+
+import Home from './Home';
 import Mouse_List from './pages/mouse/Mouse_List';
 
 const App = () => (
   <Router>
     <div>
       <ul>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/about">About</Link></li>
-        <li><Link to="/topics">Topics</Link></li>
-        <li><Link to="/mouse_list">Mouse List</Link></li>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+        <li>
+          <Link to="/topics">Topics</Link>
+        </li>
+        <li>
+          <Link to="/mouse_list">Mouse List</Link>
+        </li>
       </ul>
 
       <hr />
@@ -25,47 +33,42 @@ const App = () => (
       <Route path="/mouse_list" component={Mouse_List} />
     </div>
   </Router>
-)
-
+);
 
 const About = () => (
   <div>
     <h2>About</h2>
   </div>
-)
+);
 
 const Topics = ({ match }) => (
   <div>
     <h2>Topics</h2>
     <ul>
       <li>
-        <Link to={`${match.url}/rendering`}>
-          Rendering with React
-        </Link>
+        <Link to={`${match.url}/rendering`}>Rendering with React</Link>
       </li>
       <li>
-        <Link to={`${match.url}/components`}>
-          Components
-        </Link>
+        <Link to={`${match.url}/components`}>Components</Link>
       </li>
       <li>
-        <Link to={`${match.url}/props-v-state`}>
-          Props v. State
-        </Link>
+        <Link to={`${match.url}/props-v-state`}>Props v. State</Link>
       </li>
     </ul>
 
     <Route path={`${match.url}/:topicId`} component={Topic} />
-    <Route exact path={match.url} render={() => (
-      <h3>Please select a topic.</h3>
-    )} />
+    <Route
+      exact
+      path={match.url}
+      render={() => <h3>Please select a topic.</h3>}
+    />
   </div>
-)
+);
 
 const Topic = ({ match }) => (
   <div>
     <h3>{match.params.topicId}</h3>
   </div>
-)
+);
 
-export default App
+export default App;
--- src/pages/mouse/Mouse_List.js
+++ src/pages/mouse/Mouse_List.js
@@ -1,43 +1,67 @@
 import React, { Component } from 'react';
-import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap/dist/css/bootstrap-theme.css';
-import Button from 'react-bootstrap/lib/Button';
-
+import {
+  Button,
+  Well,
+  PageHeader,
+  Row,
+  Col,
+  Tooltip,
+  OverlayTrigger
+} from 'react-bootstrap';
+import FontAwesome from 'react-fontawesome';
 class Mouse_List extends Component {
-    render() {
-        return <div class="well bs-component">
-            <div class="page-header">
-                <h4>Mice</h4>
-            </div>
-            mouse/mouse_table
-<div class="row">
-                <div class="col-md-6">
-                    <span class="pull-left" data-toggle="tooltip" data-placement="top" title="Assign cage">
-                        <Button id="cage-mouse-button" type="button" bsSytle="btn btn-primary" data-toggle="modal" data-target="#cage-mice-together-modal" >
-                            <i class="fa fa-home fa-2x" aria-hidden="true"></i>
-                        </Button>
-                    </span>
-                    <span class="pull-left" data-toggle="tooltip" data-placement="top" title="Create breeding">
-                        <Button id="breed-mouse-button" type="button" bsSytle="btn btn-danger" >
-                            <i class="fa fa-heart fa-2x" aria-hidden="true"></i>
-                        </Button>
-                    </span>
+  render() {
+    return (
+      <Well>
+        <PageHeader>Mice</PageHeader>
+        mouse/mouse_table
+        <Row>
+          <Col md={6}>
+            <OverlayTrigger
+              placement="top"
+              overlay={<Tooltip id="tooltip-1">Assign cage</Tooltip>}
+            >
+              <Button
+                id="cage-mouse-button"
+                type="button"
+                bsStyle="primary"
+                data-toggle="modal"
+                data-target="#cage-mice-together-modal"
+              >
+                <FontAwesome name="home" size="2x" aria-hidden="true" />
+              </Button>
+            </OverlayTrigger>
+            <OverlayTrigger
+              placement="top"
+              overlay={<Tooltip id="tooltip-2">Create breeding</Tooltip>}
+            >
+              <Button id="breed-mouse-button" type="button" bsStyle="danger">
+                <FontAwesome name="heart" size="2x" aria-hidden="true" />
+              </Button>
+            </OverlayTrigger>
 
-                    <span class="pull-left" data-toggle="tooltip" data-placement="top" title="Assign status">
-                        <Button id="open-status-mouse-modal-button" type="button" bsSytle="btn btn-primary" data-toggle="modal" data-target="#set-mice-status-modal">
-                            <i class="fa fa-meh-o fa-2x" aria-hidden="true"></i>
-                        </Button>
-                    </span>
-                </div>
-                <div class="col-md-6 ">
-                    crud_buttons
-    </div>
-            </div>
-            mouse/mouse_cage_modal
-mouse/mouse_status_modal
-common/delete_confirmation_modal
-</div>
-    }
+            <OverlayTrigger
+              placement="top"
+              overlay={<Tooltip id="tooltip-3">Assign status</Tooltip>}
+            >
+              <Button
+                id="open-status-mouse-modal-button"
+                type="button"
+                bsStyle="primary"
+                data-toggle="modal"
+                data-target="#set-mice-status-modal"
+              >
+                <FontAwesome name="meh" size="2x" aria-hidden="true" />
+              </Button>
+            </OverlayTrigger>
+          </Col>
+          <Col md={6}>crud_buttons</Col>
+        </Row>
+        mouse/mouse_cage_modal mouse/mouse_status_modal
+        common/delete_confirmation_modal
+      </Well>
+    );
+  }
 }
 
 export default Mouse_List;
